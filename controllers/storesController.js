fc03ac0a61b3ce15d0d21e18ddbdfbaecdc94642
Aditya Bimawan <aditditto@gmail.com>
2021-07-21 03:05:01 +0700

    implement store CRUD

--- controllers/storesController.js
+++ controllers/storesController.js
@@ -3,11 +3,7 @@ var storeStock = require("../models/storeStock");
 
 const { body, validationResult } = require("express-validator");
 
-exports.getAllStores = (req, res, next) => {
-  res.send("TODO endpoint to get all stores");
-};
-
-exports.newStore = [
+const storeValidation = [
   body("name", "Name must not be empty.").trim().isLength({ min: 1 }).escape(),
   body("address.street", "address.street must not be empty.")
     .trim()
@@ -21,6 +17,16 @@ exports.newStore = [
     .trim()
     .isLength({ min: 1 })
     .escape(),
+];
+
+exports.getAllStores = (req, res, next) => {
+  Store.find({})
+    .then((result) => res.json(result))
+    .catch((error) => next(error));
+};
+
+exports.newStore = [
+  ...storeValidation,
   (req, res, next) => {
     const errors = validationResult(req);
 
@@ -38,7 +44,10 @@ exports.newStore = [
     })
       .save()
       .then((store) => {
-        res.status(200).json(store._id);
+        res
+          .status(201)
+          .location("/stores/" + store._id)
+          .end();
       })
       .catch((error) => {
         next(error);
@@ -47,15 +56,46 @@ exports.newStore = [
 ];
 
 exports.getStore = (req, res, next) => {
-  res.send("TODO endpoint to get store with specific ID");
+  Store.findById(req.params.id)
+    .exec()
+    .then((store) =>
+      store ? res.status(200).send(store) : res.sendStatus(404)
+    )
+    .catch((error) => next(error));
 };
 
-exports.updateStore = (req, res, next) => {
-  res.send("TODO endpoint to update a store");
-};
+exports.updateStore = [
+  ...storeValidation,
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    Store.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+      address: {
+        street: req.body.address.street,
+        district: req.body.address.district,
+        province: req.body.address.province,
+      },
+    })
+      .exec()
+      .then((store) => {
+        store ? res.sendStatus(204) : res.sendStatus(404);
+      })
+      .catch((error) => next(error));
+  },
+];
 
 exports.deleteStore = (req, res, next) => {
-  res.send("TODO endpoint to delete store");
+  Store.findByIdAndDelete(req.params.id)
+    .exec()
+    .then((store) => {
+      store ? res.sendStatus(200) : res.sendStatus(404);
+    })
+    .catch((error) => next(error));
 };
 
 exports.getAllStocks = (req, res, next) => {
