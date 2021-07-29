eab2eb23ad11951c464bc85506081a86d4b60fd1
Aditya Bimawan <aditditto@gmail.com>
2021-07-29 20:35:59 +0700

    add imgUrl to dorayaki model

--- models/dorayaki.js
+++ models/dorayaki.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 var dorayakiSchema = new Schema({
   flavor: { type: String, required: true },
   description: String,
+  imgUrl: String,
 });
-// gambar disimpan di frontend dengan nama file _id.jpg
 
 module.exports = mongoose.model("Dorayaki", dorayakiSchema);
